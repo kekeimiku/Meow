b7a09f5db2d41f44f3d25614371b16121378548c
kekeimiku <kekelanact@gmail.com>
2022-03-15 21:49:59 +0800

    121221

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-#![feature(thread_spawn_unchecked)]
 #![feature(once_cell)]
-
-use std::env;
-
-use lince::{comm::PID, mem::search_all_rw_mem};
-
 pub mod comm;
 pub mod def;
 // pub mod gui;
@@ -12,13 +6,4 @@ pub mod maps;
 pub mod mem;
 pub mod sdiff;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    unsafe {
-        PID = comm::get_pid_by_name(args[1].as_str()).unwrap()[0];
-    }
-    println!("{}", unsafe { PID });
-
-    let o = search_all_rw_mem(&1_u8.to_be_bytes());
-    println!("{:?}", o.len());
-}
+fn main() {}
