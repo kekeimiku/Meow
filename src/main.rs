8d225ed00119882d3ac6c4ca2ad847d374ec33e0
kekeimiku <kekelanact@gmail.com>
2022-03-30 19:36:09 +0800

    small change

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,28 @@
 #![feature(once_cell)]
 
-fn main() {
-    // '0x7fff5ea93408'
-    // loop {
-    //     let mut v = read_bytes(0x7fff5ea93408, 8).unwrap();
-    //     v.reverse();
-    //     let mut v1 = [0u8; 8];
-    //     v1.copy_from_slice(&v);
-    //     println!("{:?} == {}", v, i64::from_be_bytes(v1));
-    //     sleep(Duration::from_secs(1));
-    // }
-
-    // let mut xx = HashMap::new();
-    // xx.insert(1, 2);
-
+fn main() -> Result<(), std::io::Error> {
     let mut i = 0;
-
     let hello = "hello".as_bytes();
-
     loop {
-        let l = lince::mem::search_all_rw_mem(hello).unwrap();
-
-        if l.len() < 11 {
-            l.iter().for_each(|f| {
-                let a = lince::mem::read_bytes(*f, hello.len());
-                println!("{:x} -> {:?}", f, a.unwrap());
-            });
+        let t = lince::mem::search_all_rw_mem(hello)?;
+        if t.len() < 11 {
+            t.iter().try_for_each(|f| -> Result<(), std::io::Error> {
+                let a = lince::mem::read_bytes(*f, hello.len())?;
+                println!("0x{:x} -> 0x{:?}", f, a);
+                Ok(())
+            })?;
         } else {
-            l[0..10].iter().for_each(|f| {
-                let a = lince::mem::read_bytes(*f, hello.len());
-                println!("{:x} -> {:?}", f, a.unwrap());
-            });
+            t[0..10]
+                .iter()
+                .try_for_each(|f| -> Result<(), std::io::Error> {
+                    let a = lince::mem::read_bytes(*f, hello.len())?;
+                    println!("0x{:x} -> 0x{:?}", f, a);
+                    Ok(())
+                })?;
         }
 
         i += 1;
         println!("============={}===========================", i);
-        std::thread::sleep(std::time::Duration::from_secs(1))
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
-}
-
-pub fn sum(v: &[i32], n: usize) -> i32 {
-    v.iter().take(n).sum()
 }
