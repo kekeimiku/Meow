3656d7aadd5f31342cd32a00476d49cf8c18b9a4
kekeimiku <kekelanact@gmail.com>
2022-03-30 20:58:26 +0800

    small change

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-#![feature(once_cell)]
+use std::{thread::sleep, time::Duration};
 
-fn main() -> Result<(), std::io::Error> {
+use lince::mem::{read_bytes, search_all_rw_mem};
+
+fn main() {
+    loop {
+        match test1() {
+            Ok(_) => {}
+            Err(e) => {
+                println!("{}", e.to_string());
+                sleep(Duration::from_secs(1));
+            }
+        }
+    }
+}
+
+fn test1() -> Result<(), std::io::Error> {
     let mut i = 0;
     let hello = "hello".as_bytes();
     loop {
-        let t = lince::mem::search_all_rw_mem(hello)?;
+        let t = search_all_rw_mem(hello)?;
         if t.len() < 11 {
             t.iter().try_for_each(|f| -> Result<(), std::io::Error> {
-                let a = lince::mem::read_bytes(*f, hello.len())?;
-                println!("0x{:x} -> 0x{:?}", f, a);
+                let a = read_bytes(*f, hello.len())?;
+                println!("0x{:x} -> {:?}", f, a);
                 Ok(())
             })?;
         } else {
             t[0..10]
                 .iter()
                 .try_for_each(|f| -> Result<(), std::io::Error> {
-                    let a = lince::mem::read_bytes(*f, hello.len())?;
-                    println!("0x{:x} -> 0x{:?}", f, a);
+                    let a = read_bytes(*f, hello.len())?;
+                    println!("0x{:x} -> {:?}", f, a);
                     Ok(())
                 })?;
         }
 
         i += 1;
-        println!("============={}===========================", i);
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        println!("============={}=============================", i);
+        sleep(Duration::from_secs(1));
     }
 }
