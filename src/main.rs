4a5087825394ea94040f5cb7f5927f70dd3ed413
kekeimiku <kekelanact@gmail.com>
2022-04-01 12:11:49 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use std::{io::Error, thread::sleep, time::Duration};
+use std::{
+    fs::{self, File},
+    io::{Error, Read, Seek, SeekFrom},
+    path::Path,
+    thread::sleep,
+    time::Duration,
+};
 
 use lince::mem::{read_bytes, search_all_rw_mem};
 
-fn main() {    
-    loop {
-        match test1() {
-            Ok(_) => {}
-            Err(e) => {
-                println!("{}", e.to_string());
-                sleep(Duration::from_secs(1));
-            }
-        }
-    }
+fn gg() {
+    let mut i = 0;
+
+    let bytes = fs::read(Path::new("elf/tests/bin/armelf64")).unwrap();
+    let elf = elf::elf64::Elf64::new(&bytes);
+    // println!("ehdr=> {:?}", elf.ehdr());
+    println!("节区表数量：{}", elf.ehdr().e_shnum);
+    elf.phdr_iter().for_each(|f| {
+        // println!("phdr=> {:?}", f);
+    });
+
+    elf.shdr_iter().for_each(|f| {
+        println!("[{}] shdr=> {:?}", i, f);
+        i += 1;
+    })
 }
 
-fn test1() -> Result<(), Error> {
+fn main() {
+    gg();
+    // loop {
+    //     match test1() {
+    //         Ok(_) => {}
+    //         Err(e) => {
+    //             println!("{}", e.to_string());
+    //             sleep(Duration::from_secs(1));
+    //         }
+    //     }
+    // }
+}
+
+pub fn test1() -> Result<(), Error> {
     let mut i = 0;
     let hello = "hello".as_bytes();
     loop {
--- tests/tests.rs
+++
@@ -1,9 +0,0 @@
-#![feature(test)]
-
-extern crate test;
-
-#[test]
-fn bench_get_pid_by_name() {
-    use lince::comm::*;
-    assert_eq!(get_pid_by_name("kthreadd").unwrap(), 2);
-}
