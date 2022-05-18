1704eb82a4cab6071760ec51881933afa0431bf8
kekeimiku <kekelanact@gmail.com>
2022-05-18 16:58:20 +0800

    准备添加macos支持

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 #![feature(unix_chown)]
 
 pub mod error;
-
-#[cfg(target_os = "linux")]
-pub mod maps;
-
+pub mod ext;
 pub mod prompt;
 
 #[cfg(target_os = "windows")]
 pub mod windows;
+#[cfg(target_os = "windows")]
+pub mod winmaps;
 
+#[cfg(target_os = "macos")]
+pub mod macmaps;
 #[cfg(target_os = "macos")]
 pub mod macos;
 
 #[cfg(target_os = "linux")]
 pub mod linux;
-
-pub mod ext;
+#[cfg(target_os = "linux")]
+pub mod maps;
