f0818b4f7316295759fd8698be3f096a756aebe8
kekeimiku <kekelanact@gmail.com>
2022-05-21 15:58:37 +0800

    草拟马

--- src/prompt.rs
+++ src/prompt.rs
@@ -30,6 +30,37 @@ pub fn prompt(name: &str) -> Result<Vec<String>> {
     Ok(line.replace('\n', "").split_whitespace().map(String::from).collect())
 }
 
+// 吐了 TODO 等个好心人重构
+//
+macro_rules! input_scan {
+    ($app:ident,$t:expr,$input:expr) => {
+        if let Err(e) = $app.input($t, $input) {
+            println!("{}", e);
+        } else {
+            merr!($app.value_scan(), "搜索成功, 搜索到值为*的地址数: ", "搜索失败: Error: ");
+        }
+    };
+}
+
+macro_rules! match_input_type {
+    ($n1:expr,$n2:expr,$input:expr,$app:ident) => {
+        match $input[$n1] {
+            "u8" => input_scan!($app, Type::U8, $input[$n2]),
+            "u16" => input_scan!($app, Type::U16, $input[$n2]),
+            "u32" => input_scan!($app, Type::U32, $input[$n2]),
+            "u64" => input_scan!($app, Type::U64, $input[$n2]),
+            "i8" => input_scan!($app, Type::I8, $input[$n2]),
+            "i16" => input_scan!($app, Type::I16, $input[$n2]),
+            "i32" => input_scan!($app, Type::I32, $input[$n2]),
+            "i64" => input_scan!($app, Type::I64, $input[$n2]),
+            "str" => input_scan!($app, Type::STR, $input[$n2]),
+            _ => {
+                input_scan!($app, Type::UNKNOWN, $input[$n1])
+            }
+        }
+    };
+}
+
 pub fn start() -> Result<()> {
     let pid = std::env::args().nth(1).ok_or(Error::ArgsError)?.parse::<u32>()?;
     #[cfg(target_os = "linux")]
@@ -51,22 +82,32 @@ pub fn start() -> Result<()> {
                 "find" | "f" => {
                     if input.len() > 2 {
                         match input[1] {
-                            "u8" => app.input(Type::U8, input[2])?,
-                            "u16" => app.input(Type::U16, input[2])?,
-                            "u32" => app.input(Type::U32, input[2])?,
-                            "u64" => app.input(Type::U64, input[2])?,
-                            "i8" => app.input(Type::I8, input[2])?,
-                            "i16" => app.input(Type::I16, input[2])?,
-                            "i32" => app.input(Type::I32, input[2])?,
-                            "i64" => app.input(Type::I64, input[2])?,
-                            "str" => app.input(Type::STR, input[2])?,
+                            "u8" => input_scan!(app, Type::U8, input[2]),
+                            "u16" => input_scan!(app, Type::U16, input[2]),
+                            "u32" => input_scan!(app, Type::U32, input[2]),
+                            "u64" => input_scan!(app, Type::U64, input[2]),
+                            "i8" => input_scan!(app, Type::I8, input[2]),
+                            "i16" => input_scan!(app, Type::I16, input[2]),
+                            "i32" => input_scan!(app, Type::I32, input[2]),
+                            "i64" => input_scan!(app, Type::I64, input[2]),
+                            "str" => input_scan!(app, Type::STR, input[2]),
+                            "<" => {
+                                match_input_type!(3, 4, input, app);
+                                merr!(app.value_less(), "搜索成功, 搜索到值为*的地址数: ", "搜索失败: Error: ");
+                            }
+                            ">" => {
+                                match_input_type!(3, 4, input, app);
+                                merr!(app.value_more(), "搜索成功, 搜索到值为*的地址数: ", "搜索失败: Error: ");
+                            }
                             _ => {}
                         }
                     } else {
-                        app.input(Type::UNKNOWN, input[1])?;
+                        if let Err(e) = app.input(Type::UNKNOWN, input[1]) {
+                            println!("{}", e);
+                        } else {
+                            merr!(app.value_scan(), "搜索成功, 搜索到值为*的地址数: ", "搜索失败: Error: ");
+                        };
                     }
-
-                    merr!(app.value_scan(), "搜索成功, 搜索到值为*的地址数: ", "搜索失败: Error: ");
                 }
                 "write" | "w" => {
                     let addr = hexstr_to_usize(input[1])?;
@@ -96,6 +137,7 @@ pub fn start() -> Result<()> {
                     let path = input[3];
                     app.dump(addr, size, path)?;
                 }
+                "clear" | "clean" => app.clear(),
                 _ => {}
             }
         }
