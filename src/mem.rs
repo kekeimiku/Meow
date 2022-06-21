e7e49cacdbc0d10213afe74cb0f505f830d7ced9
kekeimiku <kekelanact@gmail.com>
2022-06-21 16:54:19 +0800

    remove memmem

--- src/mem.rs
+++ src/mem.rs
@@ -21,21 +21,7 @@ macro_rules! find_num_addr {
 
 // 在一个分块查找一个内存区域中值为value的地址
 // file:文件句柄, start:开始区域, end:结束区域, value:目标值,
-// flag:是否开启内存对齐搜索，默认true，
-// 如果false 则为非对齐搜索，基本只有需要搜索字符串的时候才用，搜索期间会占用较多内存，不会对文件分块
-pub fn find_region_addr(
-    file: &File,
-    mut start: usize,
-    end: usize,
-    value: &[u8],
-    flag: bool,
-) -> Result<Vec<usize>> {
-    if !flag {
-        let mut buf = vec![0; end - start];
-        file.read_at(&mut buf, start as u64)?;
-        return Ok(memchr::memmem::find_iter(&buf, value).collect::<Vec<usize>>());
-    }
-
+pub fn find_region_addr(file: &File, mut start: usize, end: usize, value: &[u8]) -> Result<Vec<usize>> {
     let mut tmp = Vec::default();
     let len = value.len();
     let mut num = 0;
@@ -45,7 +31,7 @@ pub fn find_region_addr(
         let mut buf = vec![0; size];
         file.read_at(&mut buf, start as u64)?;
         debug!("CHUNK_SIZE >= size");
-        find_num_addr!(buf,value,len,num,tmp,==);
+        find_num_addr![buf,value,len,num,tmp,==];
         return Ok(tmp.into_iter().flatten().collect::<Vec<_>>());
     }
 
@@ -53,7 +39,7 @@ pub fn find_region_addr(
 
     for _ in 0..(end - start) / CHUNK_SIZE {
         file.read_at(&mut buf, start as u64)?;
-        find_num_addr!(buf,value,len,num,tmp,==);
+        find_num_addr![buf,value,len,num,tmp,==];
         start += CHUNK_SIZE;
         num += CHUNK_SIZE;
         debug!("0..(end - start) / CHUNK_SIZE");
@@ -63,7 +49,7 @@ pub fn find_region_addr(
     if size != 0 {
         let mut buf = vec![0; size];
         file.read_at(&mut buf, start as u64)?;
-        find_num_addr!(buf,value,len,num,tmp,==);
+        find_num_addr![buf,value,len,num,tmp,==];
         debug!("(end - start) % CHUNK_SIZE");
     }
     Ok(tmp.into_iter().flatten().collect::<Vec<_>>())
