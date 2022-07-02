f66c6d8277761924909999a1f6409d389b073101
kekeimiku <kekelanact@gmail.com>
2022-07-02 20:47:44 +0800

    1111

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,12 @@ pub mod maps;
 pub mod mem;
 pub mod scan;
 pub mod value;
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+pub mod linux;
+
+#[cfg(target_os = "windows")]
+pub mod windows;
+
+#[cfg(target_os = "macos")]
+pub mod macos;
--- src/mem.rs
+++ src/mem.rs
@@ -1,39 +1,8 @@
 use crate::error::Result;
 
-use std::os::unix::prelude::FileExt;
-
 pub trait MemExt {
     fn read(&self, addr: usize, size: usize) -> Result<Vec<u8>>;
     fn write(&self, addr: usize, payload: &[u8]) -> Result<usize>;
-}
-
-pub struct Mem<'a, T: FileExt> {
-    pub handle: &'a T,
-}
-
-impl<'a, T> Mem<'a, T>
-where
-    T: FileExt,
-{
-    pub fn new(handle: &'a T) -> Mem<T> {
-        Self { handle }
-    }
-}
-
-impl<'a, T> MemExt for Mem<'a, T>
-where
-    T: FileExt,
-{
-    fn read(&self, addr: usize, size: usize) -> Result<Vec<u8>> {
-        let mut buf = vec![0; size];
-        self.handle.read_at(&mut buf, addr as u64)?;
-        Ok(buf)
-    }
-
-    fn write(&self, addr: usize, payload: &[u8]) -> Result<usize> {
-        self.handle.write_at(payload, addr as u64)?;
-        Ok(payload.len())
-    }
 }
 
 #[derive(Debug)]
